a92536ee35678a9fd51bc1009c6a647e453bd541
Dacre <enquiries@mooce.co.nz>
2018-08-20 20:37:36 +1200

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/sequmise.js',
+  mode: 'development',
+  output: {
+    filename: 'sequmise.js',
+    library: 'sequmise',
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  }
+}
