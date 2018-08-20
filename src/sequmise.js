aa04dc42a278137e881d588652c1974a03fce7da
Dacre <enquiries@mooce.co.nz>
2018-08-20 21:41:39 +1200

    Initial test suite written, Basic implementation in place

--- src/sequmise.js
+++ src/sequmise.js
@@ -1,3 +1,45 @@
-module.exports = {
+async function processTask(task) {
 
+    if (task instanceof Function) {
+        return await task()
+    } else {
+        return task
+    }
+}
+
+async function processArray(tasks) {
+
+    const results = []
+
+    while (tasks.length) {
+        const task = tasks.shift()
+
+        if (Array.isArray(task)) {
+            results.push(await processArray(task))
+        } else {
+            results.push(await processTask(task))
+        }
+    }
+
+    return results
+}
+
+module.exports = async function () {
+
+    if (arguments.length === 0) {
+        return
+    }
+
+    if (arguments.length === 1) {
+
+        if (Array.isArray(arguments[0])) {
+
+            return await processArray(arguments[0])
+        } else {
+
+            return await processTask(arguments[0])
+        }
+    }
+
+    return await processArray(Array.from(arguments))
 }
--- test/lib.js
+++ test/lib.js
@@ -1,9 +1,90 @@
+var sequmise = require('../src/sequmise');
 var assert = require('assert');
 
-describe('Array', function () {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
-            assert.equal([1, 2, 3].indexOf(4), -1);
-        });
-    });
+const delayedResult = (result, milliseconds) => async () => new Promise(resolve => setTimeout(() => resolve(result), milliseconds))
+
+describe('sequmise', function () {
+
+    describe('expected result type', function () {
+
+        it('should return promise when no arguments passed', async function () {
+            assert(sequmise() instanceof Promise)
+        })
+
+        it('should return promise when non-array argument', async function () {
+            assert(sequmise('hello') instanceof Promise)
+        })
+
+        it('should return promise with spread arguments', async function () {
+            assert(sequmise('hello', 'world', '!') instanceof Promise)
+        })
+
+        it('should return promise when empty array argument', async function () {
+            assert(sequmise([]) instanceof Promise)
+        })
+
+        it('should return promise when non-empty array argument', async function () {
+            assert(sequmise([1, 'hello', () => {}, new Promise(r => r())]) instanceof Promise)
+        })
+    })
+
+    describe('expected result values', function () {
+
+        it('should return undefined from await when no argument passed', async function () {
+
+            assert.strictEqual(await sequmise(), undefined)
+        })
+
+        it('should return value from await when non-array argument passed', async function () {
+
+            assert.strictEqual(await sequmise('hello'), 'hello')
+        })
+
+        it('should return array with spread arguments', async function () {
+
+            assert.deepEqual(await sequmise('hello', 'world', '!'), ['hello', 'world', '!'])
+        })
+
+        it('should return an empty array with empty array argument passed', async function () {
+
+            assert.deepEqual(await sequmise([]), [])
+        })
+
+        it('should return an array with result sequence matching input sequence', async function () {
+
+            assert.deepEqual(await sequmise([1, 2, 3, 4]), [1, 2, 3, 4])
+            assert.deepEqual(await sequmise(['hello', 123, 'world']), ['hello', 123, 'world'])
+            assert.deepEqual(await sequmise(['hello', () => {}, 456]), ['hello', undefined, 456])
+        })
+
+        it('should return an array with values matching sequence of resolved input promises ', async function () {
+
+            assert.deepEqual(await sequmise([
+                delayedResult('this', 10),
+                delayedResult('is', 15),
+                delayedResult('a', 20),
+                delayedResult('delayed', 25),
+                delayedResult('sequence', 30),
+            ]), ['this', 'is', 'a', 'delayed', 'sequence'])
+        })
+
+        it('should return an array with values matching sequence of resolved input promises ', async function () {
+
+            assert.deepEqual(await sequmise([
+                delayedResult('hello', 10),
+                delayedResult(123, 15),
+                delayedResult({}, 20),
+                delayedResult(undefined, 25),
+            ]), ['hello', 123, {}, undefined])
+        })
+
+        it('should return array with values matching sequence of resovled promises spread in arguments', async function () {
+
+            assert.deepEqual(await sequmise(
+                delayedResult('hello', 10),
+                delayedResult('world', 10),
+                delayedResult('!', 10)
+            ), ['hello', 'world', '!'])
+        })
+    })
 });
